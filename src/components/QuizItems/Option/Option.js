7a5c2531485496a0f5efa00983825407c6f1d58c
Riyadh <riyadh.ju@gmail.com>
2022-10-11 11:59:16 +0600

    added correct ans

--- src/components/QuizItems/Option/Option.js
+++ src/components/QuizItems/Option/Option.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-const Option = () => {
-  return <div></div>;
+const Option = ({ option, handleQuiz }) => {
+  return (
+    <div className="my-4">
+      <button
+        onClick={() => handleQuiz(option)}
+        className="bg-white py-2 px-2 border border-stone-800 rounded-md"
+      >
+        {option}
+      </button>
+    </div>
+  );
 };
 
 export default Option;
--- src/components/QuizOptions/QuizOptions.js
+++ src/components/QuizOptions/QuizOptions.js
@@ -2,19 +2,33 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
 import Option from "../QuizItems/Option/Option";
+import { click } from "@testing-library/user-event/dist/click";
 
 const QuizOptions = ({ quizQuestion }) => {
   const { id, question, options, correctAnswer } = quizQuestion;
-  console.log(quizQuestion.question);
+
+  const ShowAns = () => {
+    alert(correctAnswer);
+  };
+
+  const handleQuiz = (option) => {
+    if (correctAnswer === option) {
+      alert("this is correct");
+    } else {
+      alert("worng");
+    }
+  };
   return (
-    <div className="m-4 bg-red-400">
+    <div className="m-4 bg-red-400 py-4">
       <div className="flex justify-between items-center px-3">
         <h3>Quiz: {question}</h3>
-        <FontAwesomeIcon icon={faEye}></FontAwesomeIcon>
+        <button onClick={ShowAns}>
+          <FontAwesomeIcon icon={faEye}></FontAwesomeIcon>
+        </button>
       </div>
-      <div>
-        {options.map((option) => (
-          <Option></Option>
+      <div className="md:text-left mx-4">
+        {options.map((option, idx) => (
+          <Option option={option} key={idx} handleQuiz={handleQuiz}></Option>
         ))}
       </div>
     </div>
